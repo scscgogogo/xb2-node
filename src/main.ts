e1c3286f7bd6dbc1e257af74d2125be205d7523a
scscgogogo <20456992@qq.com>
2022-05-13 17:40:24 +0800

    增加自动编译功能

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import {Request,Response} from 'express';
 const app = express();
 const port = 3000;
+
 /**
  * 使用JSON中间件
  */
 app.use(express.json());
 
 app.listen(port, () => {
-  console.log('服务已启动！');
+  console.log('服务已启动~~');
 });
 app.get('/', (request:Request, response:Response) => {
   
