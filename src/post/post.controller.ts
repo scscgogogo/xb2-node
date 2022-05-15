9e76e4d387fba8a4b42e6b1533e2e64d95e8c266
scscgogogo <20456992@qq.com>
2022-05-15 22:03:30 +0800

    创建posts服务

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,12 +1,15 @@
-import {Request,Response,NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { getPosts } from './post.service';
+
 /**
  * 内容列表
  */
 
 export const index = (
-  request:Request,
-  response:Response,
-  next:NextFunction
-)=>{
-  response.send('内容列表接口');
-}
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const posts = getPosts();
+  response.send(posts);
+};
---
+++ src/post/post.service.ts
@@ -0,0 +1,18 @@
+/**
+ * 获取内容列表
+ */
+
+export const getPosts = () => {
+  const data = [
+    {
+      content: '会当凌绝顶，一览众山小',
+    },
+    {
+      content: '朝辞白帝彩云间，千里江陵一日还',
+    },
+    {
+      content: '飞流直下三千尺，疑是银河落九天',
+    },
+  ];
+  return data;
+};
