d78b559c7676c7bccd059f04b88764468677a1bc
scscgogogo <20456992@qq.com>
2022-05-17 16:59:13 +0800

    测试异常处理器

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -10,6 +10,9 @@ export const index = (
   response: Response,
   next: NextFunction,
 ) => {
+  if (request.headers.authorization !== 'SECRET') {
+    return next(new Error());
+  }
   const posts = getPosts();
   response.send(posts);
 };
