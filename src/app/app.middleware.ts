745e248c44b83ec0b8c737f6f1f1fc30423631d7
scscgogogo <20456992@qq.com>
2022-05-17 16:56:12 +0800

    创建了默认异常处理器

--- src/app/app.middleware.ts
+++ src/app/app.middleware.ts
@@ -10,3 +10,25 @@ export const requestUrl = (
   console.log(request.url);
   next();
 };
+
+/**
+ * 默认异常处理器
+ */
+export const defaultErrorHandler = (
+  error: any,
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  let statusCode: number, message: string;
+  /**
+   * 处理异常
+   */
+  switch (error.message) {
+    default:
+      statusCode = 500;
+      message = '服务暂时出了点问题~';
+      break;
+  }
+  response.status(statusCode).send({ message });
+};
