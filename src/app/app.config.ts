88272d9218412500ad573a15219c8dfb9bd71d91
scscgogogo <20456992@qq.com>
2022-05-19 17:25:59 +0800

    连接数据仓库服务

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,4 +5,15 @@ dotenv.config();
 /**
  * 应用配置
  */
-export const {APP_PORT}=process.env;
+export const { APP_PORT } = process.env;
+
+/**
+ * 数据仓库配置
+ */
+export const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+} = process.env;
