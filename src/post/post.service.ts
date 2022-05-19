8168e52bb4855e27844b540ae0be80981371e1f9
scscgogogo <20456992@qq.com>
2022-05-19 17:34:11 +0800

    调取内容

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,18 +1,12 @@
+import { connection } from '../app/database/mysql';
 /**
  * 获取内容列表
  */
 
-export const getPosts = () => {
-  const data = [
-    {
-      content: '会当凌绝顶，一览众山小',
-    },
-    {
-      content: '朝辞白帝彩云间，千里江陵一日还',
-    },
-    {
-      content: '飞流直下三千尺，疑是银河落九天',
-    },
-  ];
+export const getPosts = async () => {
+  const statement = `
+    SELECT * FROM post
+  `;
+  const [data] = await connection.promise().query(statement);
   return data;
 };
