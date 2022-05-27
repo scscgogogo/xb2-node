5caec25588e5157f503cf8649d0aac545d55a945
scscgogogo <20456992@qq.com>
2022-05-27 23:11:39 +0800

    增加访问控制

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -1,9 +1,10 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, response } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import * as userService from '../user/user.service';
 import { PUBLIC_KEY } from '../app/app.config';
 import { TokenPayLoad } from './auth.interface';
+import { possess } from './auth.service';
 
 /**
  * 验证用户登录数据
@@ -67,4 +68,47 @@ export const authGuard = (
   } catch (error) {
     next(new Error('UNAUTHORIZED'));
   }
+};
+
+/**
+ * 访问控制
+ */
+interface AccessControlOptions {
+  possession?: boolean;
+}
+
+export const accessControl = (options: AccessControlOptions) => {
+  return async (request: Request, response: Response, next: NextFunction) => {
+    console.log('访问控制');
+
+    //解构选项
+    const { possession } = options;
+
+    //当前用户ID
+    const { id: userId } = request.user;
+
+    //放行管理员
+    if (userId == 1) return next();
+
+    //准备资源
+    const resourceIdParam = Object.keys(request.params)[0];
+    const resourceType = resourceIdParam.replace('Id', '');
+    const resourceId = parseInt(request.params[resourceIdParam], 10);
+
+    //检查资源拥有权
+    if (possession) {
+      try {
+        const ownResource = await possess({ resourceId, resourceType, userId });
+
+        if (!ownResource) {
+          return next(new Error('USER_DOES_NOT_OWN_RESOURCE'));
+        }
+      } catch (error) {
+        return next(error);
+      }
+    }
+
+    //下一步
+    next();
+  };
 };
