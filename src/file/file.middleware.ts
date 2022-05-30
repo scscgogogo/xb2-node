d56ebd849ccebcb213dd571f6922732c91b3cbf6
scscgogogo <20456992@qq.com>
2022-05-30 22:40:44 +0800

    增加文件处理器

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction, response } from 'express';
 import multer from 'multer';
+import Jimp from 'jimp';
 
 /**
  * 创建一个Multer
@@ -12,3 +13,27 @@ const fileUpload = multer({
  * 文件拦截器
  */
 export const fileInterceptor = fileUpload.single('file');
+
+/**
+ * 文件处理器
+ */
+export const fileProcessor = async (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  //文件路径
+  const { path } = request.file;
+  let image: Jimp;
+  try {
+    //读取图形文件
+    image = await Jimp.read(path);
+  } catch (error) {
+    return next(error);
+  }
+
+  console.log(image);
+
+  //下一步
+  next();
+};
