73fc75bbed3121eeefe401e2bd452250d9143433
scscgogogo <20456992@qq.com>
2022-05-31 22:00:23 +0800

    调整图像文件的大小

--- src/file/file.middleware.ts
+++ src/file/file.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction, response } from 'express';
 import multer from 'multer';
 import Jimp from 'jimp';
+import { imageResizer } from './file.service';
 
 /**
  * 创建一个Multer
@@ -42,6 +43,8 @@ export const fileProcessor = async (
     metadata: JSON.stringify(tags),
   };
 
+  //调整图像尺寸
+  imageResizer(image, request.file);
   //下一步
   next();
 };
