d3a4beb14e89bcf4ec0bd71c7781e32b287963ac
scscgogogo <20456992@qq.com>
2022-06-30 21:05:24 +0800

    评论列表分页功能

--- src/post/post.middleware.ts
+++ src/post/post.middleware.ts
@@ -87,23 +87,21 @@ export const filter = async (
 /**
  * 内容分页
  */
-export const paginate = async (
-  request: Request,
-  response: Response,
-  next: NextFunction,
-) => {
-  // 当前页码
-  const { page = 1 } = request.query;
+export const paginate = (itemsPerPage: number) => {
+  return async (request: Request, response: Response, next: NextFunction) => {
+    // 当前页码
+    const { page = 1 } = request.query;
 
-  // 每页内容数量
-  const limit = parseInt(POSTS_PER_PAGE, 10) || 30;
+    // 每页内容数量
+    const limit = itemsPerPage || 30;
 
-  // 计算出偏移量
-  const offset = limit * (parseInt(`${page}`, 10) - 1);
+    // 计算出偏移量
+    const offset = limit * (parseInt(`${page}`, 10) - 1);
 
-  // 设置请求中的分页
-  request.pagination = { limit, offset };
+    // 设置请求中的分页
+    request.pagination = { limit, offset };
 
-  // 下一步
-  next();
+    // 下一步
+    next();
+  };
 };
