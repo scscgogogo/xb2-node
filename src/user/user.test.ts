84b62aed51d6cbfaaf805ed486ca490137fc9ac5
scscgogogo <20456992@qq.com>
2022-07-03 20:54:34 +0800

    测试用户帐户和更新账户接口

--- src/user/user.test.ts
+++ src/user/user.test.ts
@@ -75,3 +75,78 @@ describe('测试创建用户接口', () => {
     expect(response.status).toBe(201);
   });
 });
+
+/**
+ * 用户帐户
+ */
+describe('测试用户帐户接口', () => {
+  test('响应里应该包含指定的属性', async () => {
+    // 请求接口
+    const response = await request(app).get(`/users/${testUserCreated.id}`);
+
+    // 做出断言
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe(testUser.name);
+    expect(response.body).toMatchObject({
+      id: expect.any(Number),
+      name: expect.any(String),
+      avatar: null,
+    });
+  });
+
+  test('当用户不存在时，响应的状态码是 404', async () => {
+    // 请求接口
+    const response = await request(app).get('/users/-1');
+
+    // 做出断言
+    expect(response.status).toBe(404);
+  });
+});
+
+/**
+ * 更新用户
+ */
+describe('测试更新用户接口', () => {
+  test('更新用户时需要验证用户身份', async () => {
+    // 请求接口
+    const response = await request(app).patch('/users');
+
+    // 做出断言
+    expect(response.status).toBe(401);
+  });
+
+  test('更新用户数据', async () => {
+    // 签发令牌
+    const token = signToken({
+      payload: { id: testUserCreated.id, name: testUserCreated.name },
+    });
+
+    // 请求接口
+    const response = await request(app)
+      .patch('/users')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        validate: {
+          password: testUser.password,
+        },
+        update: {
+          name: testUserUpdated.name,
+          password: testUserUpdated.password,
+        },
+      });
+
+    // 调取用户
+    const user = await getUserById(testUserCreated.id, { password: true });
+
+    // 对比密码
+    const matched = await bcrypt.compare(
+      testUserUpdated.password,
+      user.password,
+    );
+
+    // 做出断言
+    expect(response.status).toBe(200);
+    expect(matched).toBeTruthy();
+    expect(user.name).toBe(testUserUpdated.name);
+  });
+});
